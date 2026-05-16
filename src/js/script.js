d9c5c42f6487389e60ac4972631ad7fe57144c84
Artem Blyshchyk <artem.bl31@gmail.com>
2026-05-16 08:53:39 +0300

    Add methods to manage active links

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,36 @@
+// ====METHOD forEach for NodeList=====
 const links = document.querySelectorAll(".section-link");
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
-    links.forEach((l) => l.classList.remove("active"));
+    links.forEach((item) => {
+      item.classList.remove("active");
+    });
     link.classList.add("active");
   });
 });
+
+// ==================METHOD ***for***===================
+// const links = document.querySelectorAll(".section-link");
+
+// for (let i = 0; i < links.length; i++) {
+//   links[i].addEventListener("click", () => {
+//     for (let j = 0; j < links.length; j++) {
+//       links[j].classList.remove("active");
+//     }
+//     links[i].classList.add("active");
+//   });
+// }
+
+// ==================METHOD ***for of***===================
+// const links = document.querySelectorAll(".section-link");
+
+// for (let link of links) {
+//   link.addEventListener("click", () => {
+//     for (let item of links) {
+//       item.classList.remove("active");
+//     }
+
+//     link.classList.add("active");
+//   });
+// }
